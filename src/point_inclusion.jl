e8faec7426e1c6a7258ab913ed58f65e0616c6b2
Alberto Mengali <disberd@gmail.com>
2025-07-07 14:14:13 +0200

    feat: make in method more generic

--- src/point_inclusion.jl
+++ src/point_inclusion.jl
@@ -1,24 +1,31 @@
-"""
-    in_exit_early(p, polys, bboxes)
-Function that checks if a point is contained one of the polyareas in vector `polys` which are associated to the bounding boxes in vector `bboxes`.
-
-Both `polys` and `bboxes` must be iterables of the same size, with element type `POLY_CART` and `BBOX_CART` respectively (both are type aliases of `Meshes.jl` types defined within `GeoBasics.jl`).
-
-This function is basically pre-filtering points by checking inclusion in the bounding box which is significantly faster than checking for the polyarea itself, especially if the polyareas is composed by a large number of points.
-
-See also [`polyareas`](@ref), [`bboxes`](@ref), [`FastInGeometry`](@ref), [`geoborders`](@ref), [`GeoBorders`](@ref).
-"""
-function in_exit_early(p, polys, bboxes)
-    T = first(polys) |> valuetype
-    p = to_cart_point(T, p)
+# This is the actual method with the actual implementation. It expects a cartesian point already as input and expects all polyareas and bboxes provided to have the same `valuetype` T as the input point
+function in_exit_early(p::POINT_CART{T}, polys, bboxes) where T
+    # Note that t
     for (poly, box) in zip(polys, bboxes)
         p in box || continue
         p in poly && return true
     end
     return false
 end
-# This is a catchall method for extension for other types
-in_exit_early(p, x) = in_exit_early(p, polyareas(Cartesian, x), bboxes(Cartesian, x))
+"""
+    in_exit_early(p, geom)
 
-Base.in(p::Union{VALID_POINT, LATLON}, geom::FastInGeometry) = in_exit_early(p, geom)
-Base.in(p::Union{VALID_POINT, LATLON}, dmn::VALID_DOMAINS) = any(el -> in_exit_early(p, el), dmn)
+Function that checks if a point is contained within the geometry `geom` using the fast point inclusion algorithm that relies on availability `geom` has valid methods for [`polyareas`](@ref) and [`bboxes`](@ref).
+
+It is expected that the input `geom` also has a valid method for `BasicTypes.valuetype` in order to extract the `T` type parameter to convert `p` to a valid cartesian point.
+
+!!! note
+    This method is basically pre-filtering points by checking inclusion in the bounding box which is significantly faster than checking for the polyarea itself, especially if the polyareas is composed by a large number of points.
+
+See also [`polyareas`](@ref), [`bboxes`](@ref), [`FastInGeometry`](@ref), [`geoborders`](@ref), [`GeoBorders`](@ref).
+"""
+in_exit_early(p, x) = in_exit_early(to_cart_point(p), polyareas(Cartesian, x), bboxes(Cartesian, x))
+in_exit_early(p, dmn::VALID_DOMAINS) = any(el -> in_exit_early(p, el), dmn)
+
+# Basic methods
+Base.in(p, geom::FastInGeometry) = in_exit_early(p, geom)
+Base.in(p, dmn::VALID_DOMAINS) = in_exit_early(p, dmn)
+
+# Additional methods to resolve ambiguities from Meshes.jl
+Base.in(p::Point, geom::FastInGeometry) = in_exit_early(p, geom)
+Base.in(p::Point, dmn::VALID_DOMAINS) = in_exit_early(p, dmn)
